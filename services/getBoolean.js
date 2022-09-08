b6fd9e480b4b23e542265c38888a0fbc57fca38c
Raúl Navarro <raul.navarro.uribe@gmail.com>
2022-09-08 14:47:34 +0200

    Generación de booleano con una promesa

--- services/getBoolean.js
+++ services/getBoolean.js
@@ -1,5 +1,12 @@
 export default function getBoolean() {
   const randomNumber = Math.round(Math.random());
   const randomBoolean = randomNumber ? true : false;
-  return randomBoolean;
+  return new Promise((resolve, reject) => {
+    if (randomBoolean !== true || false) {
+      reject(new Error("No se ha generado el booleano."));
+    }
+    setTimeout(() => {
+      resolve(randomBoolean);
+    }, 1000);
+  });
 }
